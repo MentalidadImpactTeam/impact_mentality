c1df834351c3592ba800065662bee62094c168ce
Miguel Bolanos <miguelbq88@gmail.com>
2018-11-17 18:43:37 -0800

    cambiar usuario deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:miguelbq01/impact_mentality.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/root/workspaces/rails"
+set :deploy_to, "/home/deploy/workspaces/rails"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
