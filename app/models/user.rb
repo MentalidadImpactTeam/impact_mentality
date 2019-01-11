680070eb29ab35bac4a7f38a20594ee91601ab19
Miguel Bolanos <miguelbq88@gmail.com>
2019-01-10 20:36:38 -0800

    No mostrar corrida cuando es semana de pruebas en rutinas

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_one :user_information, dependent: :destroy
   has_one :user_type
-  has_one :user_conekta_token
+  has_many :user_conekta_token
   has_many :user_conekta_subscription
 
   accepts_nested_attributes_for :user_information
